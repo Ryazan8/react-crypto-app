7b32a5e73887d679b20108ebe0a4553faae93154
Ryazan8 <88ryazanov88@gmail.com>
2025-01-10 19:43:21 +0300

    Оптимизировал вывод через object-map

--- src/components/layout/AppContent.jsx
+++ src/components/layout/AppContent.jsx
@@ -11,15 +11,18 @@ const contentStyle = {
 
 export default function AppContent() {
   const {assets, crypto} = useCrypto()
+
+  const cryptoPriceMap = crypto.reduce((acc, c) => {
+    acc[c.id] = c.price
+    return acc
+  }, {})
+
   return (
       <Layout.Content style={contentStyle}>
         <Typography.Title level={3} style={{textAlign: 'left', color: '#fff'}}>
           Portfolio:{' '}
           {assets
-              .map((asset) => {
-                const coin = crypto.find((c) => c.id === asset.id)
-                return asset.amount * coin.price
-              })
+              .map((asset) => asset.amount * cryptoPriceMap[asset.id])
               .reduce((acc, v) => (acc += v), 0).toFixed(2)}
           $
         </Typography.Title>
